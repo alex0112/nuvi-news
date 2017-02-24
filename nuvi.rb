3fa474e6834154d48bfc691c68ada90876869346
alex0112 <alarsen0112@gmail.com>
2017-02-23 20:18:01 -0700

    Refactored to include all method params as object attributes

--- nuvi.rb
+++ nuvi.rb
@@ -10,8 +10,19 @@ Zip.on_exists_proc = true ## When extracting zip files, old files will be overwr
 
 class Scraper
   attr_accessor :uri
+  attr_accessor :xml_dir
+  attr_accessor :zip_dir
+  attr_accessor :redis_key
   
-  def initialize(uri: "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/", write_dir: "tmp", limit: nil)
+  
+  def initialize(uri: "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/", zip_dir: "tmp", xml_dir: "xml", redis_key: "NEWS_XML", limit: nil)
+    ## Set some vars
+    @uri = uri
+    @zip_dir = zip_dir
+    @xml_dir = xml_dir
+    @redis_key = redis_key
+    @limit = limit
+
     ## Get the filenames
     puts 'Locating zip files at specified URI...'
     begin
@@ -33,39 +44,39 @@ class Scraper
     puts 'Located.'
 
     ## Download the files
-    puts "Downloading files to #{write_dir}..."
+    puts "Downloading files to #{zip_dir}..."
     puts "(This may take a moment)"
-    Dir.mkdir(write_dir) if !Dir.exists? write_dir
+    Dir.mkdir(zip_dir) if !Dir.exist? zip_dir
     filenames_to_uris.each do |name, uri|
       zip_data = HTTParty.get(uri).body
-      File.write("#{write_dir}/#{name}", zip_data)
-      puts "* wrote #{write_dir}/#{name} *"
+      File.write("#{zip_dir}/#{name}", zip_data)
+      puts "* wrote #{zip_dir}/#{name} *"
     end
   end
 
-  def extract_zip(read_dir: "tmp", write_dir: "xml")
-    Dir.mkdir(write_dir) if !Dir.exists? write_dir
-    Dir.foreach(read_dir).drop(2).each do |filename| ## .drop(2) because the first two elements are . and ..
+  def extract_zip()
+    Dir.mkdir(self.xml_dir) if !Dir.exists? self.xml_dir
+    Dir.foreach(self.zip_dir).drop(2).each do |filename| ## .drop(2) because the first two elements are . and ..
       puts "Extracting #{filename}"
-      file_path = File.join(read_dir, filename)
+      file_path = File.join(self.zip_dir, filename)
       puts "Current file path: #{file_path}"
       Zip::File.open(file_path) do |zipped_files|
         puts "opening zip file #{zipped_files}"
         zipped_files.each do |file|
           puts "extracting #{file.name}"
-          file.extract(File.join(write_dir, file.name))
+          file.extract(File.join(self.xml_dir, file.name))
         end
       end
       puts 'Done!'
     end 
   end
 
-  def push_xml_to_redis(read_dir: "xml", redis_key: "NEWS_XML")
+  def push_xml_to_redis()
     redis = Redis.new()
-    file_list = Dir.foreach("#{read_dir}").drop(2) ## .drop(2) because the first two elements are . and ..
+    file_list = Dir.foreach("#{self.xml_dir}").drop(2) ## .drop(2) because the first two elements are . and ..
     file_list.each do |filename|
-      file_data = File.read(File.join(read_dir, filename))
-      redis.hset(redis_key, filename, file_data)
+      file_data = File.read(File.join(self.xml_dir, filename))
+      redis.hset(self.redis_key, filename, file_data)
     end
   end
 end
